62bca5dd72bf4c0e0564ee935004369edce86b88
Joshua Wierenga <joshuagit@joshuawierenga.tech>
2022-11-06 20:19:12 +1100

    Fix GetFinalPathNameByHandle polyfill and using native functions on vista+
    Fully remove access to windows versions of GetFileInformationByHandleEx, GetFinalPathNameByHandle and CreateSymbolicLink and require using polyfill functions.
    Ensure that the polyfill functions try to use their native versions if running on vista or newer.
    
    Add cosmo support for GetLongPathName, GetVolumeInfomation, GetVolumeNameForVolumeMountPoint and GetVolumePathNameForVolumeName directly since they are supported on xp.
    
    Fixed GetFinalPathNameByHandle using wchar_t in some cases, not always returning the path and not supporting unc paths.

--- libc/nt/kernel32/GetLongPathNameW.s
+++ libc/nt/kernel32/GetLongPathNameW.s
@@ -1,2 +1,12 @@
 .include "o/libc/nt/codegen.inc"
 .imp	kernel32,__imp_GetLongPathNameW,GetLongPathNameW,0
+
+	.text.windows
+GetLongPathName:
+	push	%rbp
+	mov	%rsp,%rbp
+	.profilable
+	mov	__imp_GetLongPathNameW(%rip),%rax
+	jmp	__sysv2nt
+	.endfn	GetLongPathName,globl
+	.previous
--- libc/nt/kernel32/GetVolumeInformationW.s
+++ libc/nt/kernel32/GetVolumeInformationW.s
@@ -1,2 +1,12 @@
 .include "o/libc/nt/codegen.inc"
 .imp	kernel32,__imp_GetVolumeInformationW,GetVolumeInformationW,0
+
+	.text.windows
+GetVolumeInformation:
+	push	%rbp
+	mov	%rsp,%rbp
+	.profilable
+	mov	__imp_GetVolumeInformationW(%rip),%rax
+	jmp	__sysv2nt8
+	.endfn	GetVolumeInformation,globl
+	.previous
